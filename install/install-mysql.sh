cfad26f4aae6c19cbc530bdb7755341e019230d7
Your Name <yusuf@mesibo.com>
2022-08-01 21:06:26 +0530

    update

--- install/install-mysql.sh
+++ install/install-mysql.sh
@@ -34,6 +34,10 @@ echo "mysql securing installation..."
 
 mysql_secure_installation -u root --password=mesibo -D
 
+# Disable Password policies again since mysql_secure_installation would have enabled it
+mysql -e "uninstall plugin validate_password"
+mysql -e "UNINSTALL COMPONENT 'file://component_validate_password'"
+
 if ! grep -q mesibo /etc/my.cnf.d/mysql-server.cnf; then
 cat >> /etc/my.cnf.d/mysql-server.cnf << EOF
 
